90cf76cf135c940475800180087064d167d623ab
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-07 08:23:17 -0700

    EW google exercises

--- HW03_string1.py
+++ HW03_string1.py
@@ -25,8 +25,11 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+    if count <= 9:
+        return ('Number of donuts: ' + str(count))
+    else:
+        return 'Number of donuts: many'
+
 
 
 # B. both_ends
@@ -35,8 +38,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+    if len(s) < 2:
+        return ''    
+    else:
+        return s[0:2] + s[-2:]
 
 
 # C. fix_start
@@ -49,8 +54,11 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+    first_letter = s[0]
+    other_letters = s[1:]
+    replace = other_letters.replace(first_letter,'*')
+    return first_letter + replace    
+
 
 
 # D. MixUp
@@ -61,8 +69,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+    new_a = b[0:2] + a[2:]
+    new_b = a[0:2] + b[2:]
+    return new_a + ' ' + new_b
 
 
 # Provided simple test() function used in main() to print
